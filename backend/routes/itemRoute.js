661fb441ef1973acb1f0776b5060d1567bf68113
Regine <ginou10@msn.com>
2022-04-21 19:42:00 -0400

    item post service fix

--- backend/routes/itemRoute.js
+++ backend/routes/itemRoute.js
@@ -1,112 +1,112 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const authenticateToken = require('../util/authenticate');
+const authenticateToken = require("../util/authenticate");
 
-const { itemService } = require('../services');
+const { itemService } = require("../services");
 
-router.get('/', async (req, res, next) => {
-    try{
-        let result = await itemService.findAll();
-        if(result && result.length > 0){
-            return res.status(200).json(result);
-        } else {
-            return res.status(200).json([]);
-        }
-    } catch (e){
-        console.log(e);
-        return res.status(500).json({
-            message: "Error occured while retrieving all items"
-        })
+router.get("/", async (req, res, next) => {
+  try {
+    let result = await itemService.findAll();
+    if (result && result.length > 0) {
+      return res.status(200).json(result);
+    } else {
+      return res.status(200).json([]);
     }
-})
+  } catch (e) {
+    console.log(e);
+    return res.status(500).json({
+      message: "Error occured while retrieving all items",
+    });
+  }
+});
 
-router.get('/:id', async (req, res, next) => {
-    try{
-        let id = req.params.id;
-        let result = await itemService.findById(id);
-        if(result && result.length > 0){
-            return res.status(200).json(result)
-        } else {
-            return res.status(404).json({
-                message: "item not found!"
-            })
-        }
-    } catch (e) {
-        console.log(e);
-        res.status(500).json({
-            message: "Error occured while retrieving item " + req.params.id
-        })
+router.get("/:id", async (req, res, next) => {
+  try {
+    let id = req.params.id;
+    let result = await itemService.findById(id);
+    if (result && result.length > 0) {
+      return res.status(200).json(result);
+    } else {
+      return res.status(404).json({
+        message: "item not found!",
+      });
     }
-})
+  } catch (e) {
+    console.log(e);
+    res.status(500).json({
+      message: "Error occured while retrieving item " + req.params.id,
+    });
+  }
+});
 
-router.get('/search/:key', async (req, res, next) => {
-    try{
-        let search_str = req.params.key;
-        let result = await itemService.search(search_str);
-        return res.status(200).json(result);
-    } catch (e) {
-        console.log(e);
-        res.status(500).send({
-            message: "Error occured while searching for item"
-        })
-    }
-})
+router.get("/search/:key", async (req, res, next) => {
+  try {
+    let search_str = req.params.key;
+    let result = await itemService.search(search_str);
+    return res.status(200).json(result);
+  } catch (e) {
+    console.log(e);
+    res.status(500).send({
+      message: "Error occured while searching for item",
+    });
+  }
+});
 
-router.post('/', async (req, res, next) => {
-    try {
-        let newItem = req.body;
-        let result = await itemService.create(newItem);
-        return res.status(200).json({
-            id: result
-        })
-    } catch (e) {
-        console.log(e);
-        res.status(500).json({
-            message: "Error occured while creating item"
-        })
-    }
-})
+router.post("/", async (req, res, next) => {
+  try {
+    let newItem = req.body;
+    let result = await itemService.create(newItem);
+    return res.status(200).json({
+      id: result,
+    });
+  } catch (e) {
+    console.log(e);
+    res.status(500).json({
+      message: "Error occured while creating item",
+    });
+  }
+});
 
-router.put('/', async (req, res, next) => {
-    try {
-        let newItem = req.body;
-        let result = await itemService.update(newItem);
-        if(result.affectedRows == 1){
-            return res.status(200).json({
-                message: "Item updated"
-            })
-        } else {
-            return res.status(404).json({
-                message: "Item not found/not updated"
-            })
-        }
-    } catch (e) {
-        console.log(e);
-        res.status(500).json({
-            message: "Error occured while updating item"
-        })
+router.put("/", async (req, res, next) => {
+  try {
+    let newItem = req.body;
+    let result = await itemService.update(newItem);
+    if (result.affectedRows == 1) {
+      return res.status(200).json({
+        message: "Item updated",
+      });
+    } else {
+      return res.status(404).json({
+        message: "Item not found/not updated",
+      });
     }
-})
+  } catch (e) {
+    console.log(e);
+    res.status(500).json({
+      message: "Error occured while updating item",
+    });
+  }
+});
 
-router.delete('/:id', async (req, res, next) => {
-    try {
-        let id = req.params.id;
-        let result = await itemService.delete(id);
-        if(result.affectedRows == 1){
-            return res.status(200).json({
-                message: "Item deleted"
-            })
-        } else {
-            return res.status(404).json({
-                message: "Item not found"
-            })
-        }
-    } catch (e) {
-        console.log(e);
-        return res.status(500).json({
-            message: "Error occured while deleting item"
-        })
+router.delete("/:id", async (req, res, next) => {
+  try {
+    let id = req.params.id;
+    let result = await itemService.delete(id);
+    if (result.affectedRows == 1) {
+      return res.status(200).json({
+        message: "Item deleted",
+      });
+    } else {
+      return res.status(404).json({
+        message: "Item not found",
+      });
     }
-})
+  } catch (e) {
+    console.log(e);
+    return res.status(500).json({
+      message: "Error occured while deleting item",
+    });
+  }
+});
 
 module.exports = router;
--- backend/services/item.service.js
+++ backend/services/item.service.js
@@ -1,52 +1,53 @@
-const pool = require('../db/database');
+const pool = require("../db/database");
 
 const itemService = {
-    create: async (item) => {
-        let sql = 'INSERT INTO item (name, description, count, description, location) VALUES (?, ?, ?, ?, ?)';
-        const [results, fields] = await pool.promise().execute(sql, [
-            item.name,
-            item.description
-        ]);
-        return results;
-    },
+  create: async (item) => {
+    let sql =
+      "INSERT INTO item (name, description, count, location) VALUES (?, ?, ?, ?)";
+    const [results, fields] = await pool
+      .promise()
+      .execute(sql, [item.name, item.description, item.count, item.location]);
+    return results;
+  },
 
-    findById: async (id) => {
-        let sql = 'SELECT * FROM item WHERE id = ?';
-        const [results, fields] = await pool.promise().execute(sql, [id]);
-        return results;
-    },
+  findById: async (id) => {
+    let sql = "SELECT * FROM item WHERE id = ?";
+    const [results, fields] = await pool.promise().execute(sql, [id]);
+    return results;
+  },
 
-    findAll: async () => {
-        let sql = 'SELECT * FROM item';
-        const [results, fields] = await pool.promise().execute(sql);
-        return results;
-    },
+  findAll: async () => {
+    let sql = "SELECT * FROM item";
+    const [results, fields] = await pool.promise().execute(sql);
+    return results;
+  },
 
-    search: async (search_str) => {
-        let sql = 'SELECT item.id, item.name, item.description, item.count, item.location' +
-        'FROM items' + 
-        'WHERE CONCAT(name, description) LIKE ? ' +
-        'GROUP BY item.id';
-        const [results, fields] = await pool.promise().execute(sql, ['%'+search_str+'%']);
-        return results;
-    },
+  search: async (search_str) => {
+    let sql =
+      "SELECT item.id, item.name, item.description, item.count, item.location" +
+      "FROM items" +
+      "WHERE CONCAT(name, description) LIKE ? " +
+      "GROUP BY item.id";
+    const [results, fields] = await pool
+      .promise()
+      .execute(sql, ["%" + search_str + "%"]);
+    return results;
+  },
 
-    update: async (item) => {
-        let sql = 'UPDATE item SET name = ?, description = ?, count = ?, location = ?';
-        const [results, fields] = await pool.promise().execute(sql, [
-            item.name,
-            item.description,
-            item.count,
-            item.location
-        ]);
-        return results;
-    },
+  update: async (item) => {
+    let sql =
+      "UPDATE item SET name = ?, description = ?, count = ?, location = ?";
+    const [results, fields] = await pool
+      .promise()
+      .execute(sql, [item.name, item.description, item.count, item.location]);
+    return results;
+  },
 
-    delete: async (id) => {
-        let sql = 'DELETE FROM item WHERE id = ?';
-        const [results, fields] = await pool.promise().execute(sql, [id]);
-        return results;
-    }
-}
+  delete: async (id) => {
+    let sql = "DELETE FROM item WHERE id = ?";
+    const [results, fields] = await pool.promise().execute(sql, [id]);
+    return results;
+  },
+};
 
 module.exports = itemService;
